131ae5e1251ad0ca13efa7aa2f804dd8f1754d7f
Katherynn Gomez <90471571+KoShLaBs@users.noreply.github.com>
2025-10-29 00:20:39 -0500

    Refactor Vehiculo class to use 'placa' instead of 'marca'

--- Vehiculo.java
+++ Vehiculo.java
@@ -1,38 +1,49 @@
 public class Vehiculo {
-    public String marca;
+    public String placa;
     public String modelo;
     public int capacidad;
-    
-    //Constructor
-    public Vehiculo(String marca, String modelo, int capacidad) {
-        this.marca = marca;
+
+    // Constructor
+    public Vehiculo(String placa, String modelo, int capacidad) {
+        this.placa = placa;
         this.modelo = modelo;
         this.capacidad = capacidad;
     }
-    // Constructor vacio 
-    public Vehiculo(){
+
+    // Constructor vacio
+    public Vehiculo() {
     }
-    //Getters 
-    public String getMarca() {
-        return marca;
+
+    // Getters
+    public String getPlaca() {
+        return placa;
     }
+
     public String getModelo() {
         return modelo;
     }
+
     public int getCapacidad() {
         return capacidad;
     }
-    // Setters 
-    public void setMarca(String marca) {
-        this.marca = marca;
+
+    // Setters
+    public void setPlaca(String placa) {
+        this.placa = placa;
     }
+
     public void setModelo(String modelo) {
         this.modelo = modelo;
     }
+
     public void setCapacidad(int capacidad) {
         this.capacidad = capacidad;
     }
-    
-    
+
+    // Metodo
+    public boolean buscarPlaca(String placaBuscar) {
+        return this.placa == placaBuscar;
+    }
+
 }
 
